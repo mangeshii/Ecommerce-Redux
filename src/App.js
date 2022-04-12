90bb04b90795b9c56e3e6f33e2bef020485c86d7
mangeshipatil <thisismangeshi@gmail.com>
2022-04-12 11:11:18 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
+import ProductDetails from './components/ProductDetails';
+import ProductListing from './components/ProductListing';
 
 
 function App() {
   return (
     <div className="App">
-      hello
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productid" exact component={ProductDetails} />
+        </Switch>
+      </Router>
     </div>
   );
 }
