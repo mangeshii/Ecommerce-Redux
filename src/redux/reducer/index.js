21576e3ee91f82e255d46e1a3a9767cbcee53904
mangeshipatil <thisismangeshi@gmail.com>
2022-04-14 16:24:48 +0530

    added selected product reducer in rootreducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux"; 
-import { ProductReducer } from "./productReducer"; 
+import { ProductReducer,SelectedProductReducer } from "./productReducer"; 
 
 
 export const rootReducer= combineReducers({
-    ProductReducer: ProductReducer
+    ProductReducer: ProductReducer,
+    SelectedProductReducer:SelectedProductReducer
 })
 
